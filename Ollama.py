275bcc0503b5ece97bf2f201cf482d995957dd45
notansooon <ansonjiang043@gmail.com>
2025-01-21 01:33:46 -0500

    added data

--- Ollama.py
+++ Ollama.py
@@ -2,3 +2,7 @@ import Ollama
 import streamlit as st
 
 
+
+from langchain_community.document_loaders import UnstructuredPDFLoader
+from langchain_community.document_loaders import OnlinePDFLoader
+
